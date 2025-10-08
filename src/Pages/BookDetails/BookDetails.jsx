f938ac9cbe80dd8d374395aec34148cfa614665b
akib-naimul <akibnaimul2000@gmail.com>
2025-10-08 18:37:10 +0600

    get the data for BookDetails from Routes using loader

--- src/Pages/BookDetails/BookDetails.jsx
+++ src/Pages/BookDetails/BookDetails.jsx
@@ -1,6 +1,15 @@
 import React from 'react';
+import { useLoaderData, useParams } from 'react-router';
 
 const BookDetails = () => {
+    const {bookId} = useParams();
+    const id = parseInt(bookId);
+    const data = useLoaderData();
+    // console.log(data);
+
+    const singleBook = data.find(book => book.bookId === id);
+    console.log(singleBook);
+
     return (
         <div>
             <h1>This is the Book Details Page</h1>
