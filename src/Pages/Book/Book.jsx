d8035bf887db9420eac14756cda28989d518ab1f
akib-naimul <akibnaimul2000@gmail.com>
2025-10-08 18:17:23 +0600

    Dynamically book cart as a link to bookDetails added

--- src/Pages/Book/Book.jsx
+++ src/Pages/Book/Book.jsx
@@ -1,29 +1,31 @@
 import React, { use } from 'react';
+import { Link } from 'react-router';
 
 const Book = ({ singleBook }) => {
     // const data = use(bookPromise)
     // console.log(singleBook);
-    const { author, bookId, bookName, category,image,publisher,rating, review, tags, totalPages,yearOfPublishing
- } = singleBook;
+    const { bookId, bookName, image } = singleBook;
     return (
-        <div className="card bg-base-100 w-96 shadow-sm">
-            <figure>
-                <img
-                    src= {image}
-                    alt="Shoes" />
-            </figure>
-            <div className="card-body">
-                <h2 className="card-title">
-                    Card Title
-                    <div className="badge badge-secondary">NEW</div>
-                </h2>
-                <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
-                <div className="card-actions justify-end">
-                    <div className="badge badge-outline">Fashion</div>
-                    <div className="badge badge-outline">Products</div>
+        <Link to={`/bookDetails/${bookId}`}>
+            <div className="card bg-base-100 w-96 shadow-sm border">
+                <figure className='px-10 pt-10 bg-grey-200'>
+                    <img className='h-72 '
+                        src={image}
+                        alt="Shoes" />
+                </figure>
+                <div className="card-body">
+                    <h2 className="card-title">
+                        {bookName}
+                        <div className="badge badge-secondary">NEW</div>
+                    </h2>
+                    <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
+                    <div className="card-actions justify-end">
+                        <div className="badge badge-outline">{bookId}</div>
+                        <div className="badge badge-outline">Products</div>
+                    </div>
                 </div>
             </div>
-        </div>
+        </Link>
     );
 };
 
---
+++ src/Pages/BookDetails/BookDetails.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const BookDetails = () => {
+    return (
+        <div>
+            <h1>This is the Book Details Page</h1>
+        </div>
+    );
+};
+
+export default BookDetails;
