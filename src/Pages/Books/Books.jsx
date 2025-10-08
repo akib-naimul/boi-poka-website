c4be5df755130e65d3cf55d85d59038f7b4fc67b
akib-naimul <akibnaimul2000@gmail.com>
2025-10-08 17:13:52 +0600

    books card added from json data

--- src/Pages/Books/Books.jsx
+++ src/Pages/Books/Books.jsx
@@ -1,7 +1,7 @@
 import React, { Suspense, useState } from 'react';
 import Book from '../Book/Book';
 
-const Books = ({data}) => {
+const Books = ({ data }) => {
     // const [allBooks, setAllBooks] = useState([]);
 
     // const bookPromise = fetch("booksData.json")
@@ -12,9 +12,11 @@ const Books = ({data}) => {
         <div className=' text-3xl text-center p-6'>
             <h1>This is the Books Page</h1>
             <Suspense fallback={<p>Loadding....</p>}>
-                {
-                    data.map(singleBook => <Book key={singleBook.bookId} singleBook={singleBook}></Book>)
-                }
+                <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 m-6'>
+                    {
+                        data.map(singleBook => <Book key={singleBook.bookId} singleBook={singleBook}></Book>)
+                    }
+                </div>
             </Suspense>
         </div>
     );
